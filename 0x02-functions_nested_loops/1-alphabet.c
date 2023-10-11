c014b53aabf5c01acf566a4bc21b7524535fae8b
Sholafunmi <sholafunmiadewara@gmail.com>
2023-10-11 09:34:59 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+	return (0);
+}
