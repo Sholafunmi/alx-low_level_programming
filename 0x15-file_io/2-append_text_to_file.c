3c0010d57b1a216a462cd510af67729611571540
Sholafunmi <sholafunmiadewara@gmail.com>
2023-11-27 07:41:59 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,38 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
- * append_text_to_file -To appends text at the end of a file
- * @filename: This is the file to append the text to
- * @text_content: content to append into the file
+ * append_text_to_file -To  creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
  *
- * Return: 1 on success and -1 on failure
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, a, b = 0;
+	int file;
+	ssize_t length = 0, inlen = 0;
+	char *ptr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (text_content[b])
-			b++;
-		a = write(fd, text_content, b);
-		if (a != b)
-			return (-1);
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
 	}
 
-	close(fd);
-
+	if (close(file) == -1 || inlen != length)
+		return (-1);
 	return (1);
 }
